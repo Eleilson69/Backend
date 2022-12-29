67242df7e5a5a687314f053ae93f7a6fe843e5c3
ELEILSON SANTOS <105988025+Eleilson69@users.noreply.github.com>
2022-12-29 15:09:40 -0300

    update

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
-const filmes = require('./src/data/filmes.json')
+const morgan = require('morgan');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const filmes = require('./src/data/filmes.json');
 
 const app = express();
-const port = process.env.PORT || 3000;
 
-app.get('/filmes', (req, res) => {
-    return res.json(filmes)
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.json());
+app.use(cors());
+
+app.get('/', (req, res) => {
+    return res.json(filmes);
 })
 
-app.listen(port, () => {
-    console.log(`servidor está rodando`)
+app.listen(21262, () => {
+    console.log('Express startd at http://localhost:21262');
 })
